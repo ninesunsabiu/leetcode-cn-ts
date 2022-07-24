6788c5aa0f15f38ce715b0b0b09fed375e23cff0
Ezio Lin <ninesuns.lin@outlook.com>
2022-07-24 17:21:27 +0800

    🚧 1184 换另一种方式实现内环和外环距离的直接计算
    内环 + 外环 = 整圈距离和
    整圈距离和 = distance 总和
    内环和 = 小起点到大终点的和

--- 1184.公交站间的距离.ts
+++ 1184.公交站间的距离.ts
@@ -9,14 +9,18 @@ function distanceBetweenBusStops(distance: number[], start: number, destination:
     // 交换起点和终点 使得起点小于终点 并不会影响结果
     [start, destination] = start <= destination ? [start, destination] : [destination, start];
 
-    const sum = (s: number, e: number) => distance.slice(s, e).reduce((a, b) => a + b, 0);
+    const sum = (s: number, e: number) => {
+        let sum = 0;
+        for (let i = s; i < e; i++) {
+            sum = distance[i] + sum | 0;
+        }
+        return sum;
+    };
 
-    // 内环距离和
-    const a = sum(start, destination);
-    // 外圈距离和
-    const b = sum(0, start) + sum(destination, distance.length);
+    const all = sum(0, distance.length);
+    const inner = sum(start, destination);
 
-    return Math.min(a, b)
+    return Math.min(inner, all - inner | 0)
 }
 // @lc code=end
 
