f62e9f14affb1b42ba010811283f1a65ec58e663
Ezio Lin <ninesuns.lin@outlook.com>
2022-08-13 11:17:01 +0800

    🚧 242 排序后判断相等

---
+++ 242.有效的字母异位词.ts
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=242 lang=typescript
+ *
+ * [242] 有效的字母异位词
+ *
+ * https://leetcode.cn/problems/valid-anagram/description/
+ *
+ * algorithms
+ * Easy (65.57%)
+ * Likes:    640
+ * Dislikes: 0
+ * Total Accepted:    471K
+ * Total Submissions: 718K
+ * Testcase Example:  '"anagram"\n"nagaram"'
+ *
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ * 
+ * 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 1 
+ * s 和 t 仅包含小写字母
+ * 
+ * 
+ * 
+ * 
+ * 进阶: 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+ * 
+ */
+
+// @lc code=start
+function isAnagram(s: string, t: string): boolean {
+    // 字母排序后相等
+    const sortedStr = (s: string) => Array.from(s).sort().join('');
+    return sortedStr(s) === sortedStr(t); 
+};
+// @lc code=end
+export {}
+
