59b5563dbd43ef779334829e2c883365ec881442
Ezio Lin <ninesuns.lin@outlook.com>
2022-09-06 23:17:25 +0800

    🚧 828 参考题解方案解题 计算一个贡献度

---
+++ 828.统计子串中的唯一字符.ts
@@ -0,0 +1,153 @@
+/*
+ * @lc app=leetcode.cn id=828 lang=typescript
+ *
+ * [828] 统计子串中的唯一字符
+ *
+ * https://leetcode.cn/problems/count-unique-characters-of-all-substrings-of-a-given-string/description/
+ *
+ * algorithms
+ * Hard (55.78%)
+ * Likes:    260
+ * Dislikes: 0
+ * Total Accepted:    22K
+ * Total Submissions: 34.2K
+ * Testcase Example:  '"ABC"'
+ *
+ * 我们定义了一个函数 countUniqueChars(s) 来统计字符串 s 中的唯一字符，并返回唯一字符的个数。
+ * 
+ * 例如：s = "LEETCODE" ，则其中 "L", "T","C","O","D" 都是唯一字符，因为它们只出现一次，所以
+ * countUniqueChars(s) = 5 。
+ * 
+ * 本题将会给你一个字符串 s ，我们需要返回 countUniqueChars(t) 的总和，其中 t 是 s 的子字符串。输入用例保证返回值为 32
+ * 位整数。
+ * 
+ * 注意，某些子字符串可能是重复的，但你统计时也必须算上这些重复的子字符串（也就是说，你必须统计 s 的所有子字符串中的唯一字符）。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入: s = "ABC"
+ * 输出: 10
+ * 解释: 所有可能的子串为："A","B","C","AB","BC" 和 "ABC"。
+ * ⁠    其中，每一个子串都由独特字符构成。
+ * ⁠    所以其长度总和为：1 + 1 + 1 + 2 + 2 + 3 = 10
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入: s = "ABA"
+ * 输出: 8
+ * 解释: 除了 countUniqueChars("ABA") = 1 之外，其余与示例 1 相同。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：s = "LEETCODE"
+ * 输出：92
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= s.length <= 10^5
+ * s 只包含大写英文字符
+ * 
+ * 
+ */
+export {}
+// @lc code=start
+function uniqueLetterString(s: string): number {
+
+    const getContributionCountFn = getContributionCount(s.length)
+
+    return ofLazyPipe(() => Array.from(s))
+        .pipe(getAllCharIndexArray)
+        .pipe(
+            it => it.map(getContributionCountFn)
+        )
+        .pipe(sumNumberArray)
+        .calc()
+};
+
+type Char = string;
+
+/**
+ * 得到一个项在数组中的所有索引位置
+ */
+const getItemIndex = (char: Char, s: Array<Char>): Array<number> => {
+    return s.flatMap(
+        (c, idx) => {
+            return c === char ? [idx] : []
+        }
+    )
+}
+
+const upperCaseAlphaASCIICode = 'A'.charCodeAt(0)
+
+/**
+ * 获得一个 26 个大写字母的数组  
+ * 该数组的第 i 项，代表第 i 个字母在给定字符串中的所有索引位置
+ */
+const getAllCharIndexArray = (s: Array<Char>): Array<Array<number>> => {
+    const ans = Array.from({ length: 26 }, (): Array<number> => [])
+
+    for (const c of s) {
+        const idxOfAns = c.charCodeAt(0) - upperCaseAlphaASCIICode
+        const arrayInAns = ans[idxOfAns] ?? []
+        if (arrayInAns.length === 0) {
+            // 没有被统计过 进行统计
+            ans[idxOfAns] = getItemIndex(c, s)
+        } else {
+            // 已被统计过 直接跳过
+            continue
+        }
+    }
+
+    return ans
+
+}
+
+/**
+ * 根据间隔索引范围，计算一个某个字符的贡献度
+ */
+const getContributionCount = (endForRight: number) => {
+    return (indexInterval: Array<number>) => {
+        // 当 indexInterval 的 0 项做差值时，其前一个索引处为 -1
+        const startOfLeft = -1
+        let ans = 0
+        for (const [idx, point] of indexInterval.entries()) {
+            // 前一项
+            const prevPoint = indexInterval[idx - 1] ?? startOfLeft
+            const successorPoint = indexInterval[idx + 1] ?? endForRight
+            ans += (point - prevPoint) * (successorPoint - point)
+        }
+        return ans
+    }
+}
+
+
+const sumInt = (a: number, b: number) => a + b | 0
+const sumNumberArray = (as: Array<number>) => as.reduce(sumInt, 0)
+
+type Pipeable<A> = {
+    pipe: <B>(fn: (a: A) => B) => Pipeable<B>
+    calc: () => A
+}
+
+
+const ofLazyPipe = <A>(lazyInput: () => A): Pipeable<A> => {
+    return {
+        pipe: <B>(fn: (a: A) => B) => ofLazyPipe(() => fn(lazyInput())),
+        calc: lazyInput
+    }
+}
+
+
+// @lc code=end
+
