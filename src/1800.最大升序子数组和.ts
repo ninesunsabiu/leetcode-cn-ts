bc0f7c820f3e54b22f2bceb63feb405c4e6bbea3
Ezio Lin <ninesuns.lin@outlook.com>
2022-10-07 11:36:44 +0800

    🚧 简单模拟

---
+++ src/1800.最大升序子数组和.ts
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=1800 lang=typescript
+ *
+ * [1800] 最大升序子数组和
+ *
+ * https://leetcode.cn/problems/maximum-ascending-subarray-sum/description/
+ *
+ * algorithms
+ * Easy (69.38%)
+ * Likes:    58
+ * Dislikes: 0
+ * Total Accepted:    24.5K
+ * Total Submissions: 35.3K
+ * Testcase Example:  '[10,20,30,5,10,50]'
+ *
+ * 给你一个正整数组成的数组 nums ，返回 nums 中一个 升序 子数组的最大可能元素和。
+ * 
+ * 子数组是数组中的一个连续数字序列。
+ * 
+ * 已知子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，若对所有 i（l ），numsi  < numsi+1
+ * 都成立，则称这一子数组为 升序 子数组。注意，大小为 1 的子数组也视作 升序 子数组。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [10,20,30,5,10,50]
+ * 输出：65
+ * 解释：[5,10,50] 是元素和最大的升序子数组，最大元素和为 65 。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [10,20,30,40,50]
+ * 输出：150
+ * 解释：[10,20,30,40,50] 是元素和最大的升序子数组，最大元素和为 150 。 
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums = [12,17,15,13,10,11,12]
+ * 输出：33
+ * 解释：[10,11,12] 是元素和最大的升序子数组，最大元素和为 33 。 
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：nums = [100,10,1]
+ * 输出：100
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 1 
+ * 
+ * 
+ */
+export {}
+// @lc code=start
+type NonEmptyArray<T> = [T, ...Array<T>]
+const isNonEmptyArray = <T>(it: Array<T>): it is NonEmptyArray<T> => it.length > 0
+
+function maxAscendingSum(nums: NonEmptyArray<number>): number {
+    const program = (ans: number, prev: Record<"sum"|"item", number>, nums: Array<number>): number => {
+        if (isNonEmptyArray(nums)) {
+            const [hd, ...tail] = nums
+            const { sum, item } = prev
+            if (item < hd) {
+                // 构成升序 subarray 继续累加和
+                return program(ans, { sum: sum + hd | 0, item: hd }, tail)
+            } else {
+                // 不再构成升序，结算一次和 并求出最大值
+                const newAns = Math.max(ans, sum)
+                return program(newAns, { sum: hd, item: hd }, tail)
+            }
+        } else {
+            return Math.max(ans, prev.sum)
+        }
+    }
+
+    const [hd, ...tail] = nums
+    return program(-1, { sum: hd, item: hd }, tail)
+};
+// @lc code=end
+
