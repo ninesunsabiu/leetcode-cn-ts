6b9a983af4450b07194dc4c1270b999b45f7f4cb
Ezio Lin <ninesuns.lin@outlook.com>
2022-11-03 21:56:47 +0800

    🚧 枚举模拟 从最大可能的重复子串开始尝试

---
+++ src/1668.最大重复子字符串.ts
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=1668 lang=typescript
+ *
+ * [1668] 最大重复子字符串
+ *
+ * https://leetcode.cn/problems/maximum-repeating-substring/description/
+ *
+ * algorithms
+ * Easy (44.39%)
+ * Likes:    116
+ * Dislikes: 0
+ * Total Accepted:    34.5K
+ * Total Submissions: 73.4K
+ * Testcase Example:  '"ababc"\n"ab"'
+ *
+ * 给你一个字符串 sequence ，如果字符串 word 连续重复 k 次形成的字符串是 sequence 的一个子字符串，那么单词 word 的
+ * 重复值为 k 。单词 word 的 最大重复值 是单词 word 在 sequence 中最大的重复值。如果 word 不是 sequence
+ * 的子串，那么重复值 k 为 0 。
+ * 
+ * 给你一个字符串 sequence 和 word ，请你返回 最大重复值 k 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：sequence = "ababc", word = "ab"
+ * 输出：2
+ * 解释："abab" 是 "ababc" 的子字符串。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：sequence = "ababc", word = "ba"
+ * 输出：1
+ * 解释："ba" 是 "ababc" 的子字符串，但 "baba" 不是 "ababc" 的子字符串。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：sequence = "ababc", word = "ac"
+ * 输出：0
+ * 解释："ac" 不是 "ababc" 的子字符串。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 1 
+ * sequence 和 word 都只包含小写英文字母。
+ * 
+ * 
+ */
+export {}
+// @lc code=start
+function maxRepeating(sequence: string, word: string): number {
+    const patternLen = word.length
+    const maxK = sequence.length / patternLen | 0
+    return (Array.from({ length: maxK }, (_, i) => word.repeat(maxK - i))
+        .find(it => sequence.includes(it))?.length ?? 0) / patternLen
+};
+// @lc code=end
+
